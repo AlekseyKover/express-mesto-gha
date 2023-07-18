972f17476d4bb8c8d5dd54f9f57d5473c7bc618c
AlekseyKover <109689797+AlekseyKover@users.noreply.github.com>
2023-07-18 17:48:37 +0300

    test5

--- routes/users.js
+++ routes/users.js
@@ -19,8 +19,11 @@ router.get('/:userId', celebrate({
 }), getUserById);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required,
-    about: Joi.string().min(2).max(30).required,
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(links),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
   }),
 }), updateUser);
 router.patch('/me/avatar', celebrate({
